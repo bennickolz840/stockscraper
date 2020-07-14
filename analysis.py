db0c3b4ba8af2ff1329fbce05757e7664070a4a7
Benedict Nicholas <benedict.nicholas@flextrade.com>
2020-07-14 18:16:02 +0100

    Added average calculator functionality

--- analysis.py
+++ analysis.py
@@ -2,47 +2,65 @@ import numpy as np
 import os
 from scraper import stock
 from bs4 import BeautifulSoup
-#history = np.loadtxt('stockhistory.txt', skiprows = 1)
-os.chdir(r'C:\Users\bnichola\Git_Projects\stock_scraper')
-firstBuys = np.loadtxt('initialBuys.txt', dtype = str, delimiter=',')
-symbols = firstBuys[:,1]
-urls = firstBuys[:,3]
-initialBuys = firstBuys[:,2]
-#prices = history[:,1]
-#times = history[:,2]
- 
-for i in range(0, len(symbols)):
-    stocks = stock(urls[i], symbols[i], initialBuys[i])
-    price = stocks.priceScraper(stocks.url)
-    if stocks.initialBuys != "NA":
-        gain = (price - float(stocks.initialBuys)) / float(stocks.initialBuys)
-        #print (stocks.symbol + " " + str(price) + " " + str(gain))
-        if gain > 0.05:
-            print("SELL " + stocks.symbol + " " + str("%2f" % gain) + " " + str(price))
-#           firstBuys[i,2] = "NA"
-##            np.savetxt('initialBuys.txt', firstBuys, fmt='%s', delimiter= ',')
-    stocks.historyBuilder(stocks.symbol, price)
-    history = np.loadtxt('stockhistory.txt', dtype = str, skiprows = 1, delimiter = ',')
-    symbolList = history[:,0]
-    symbolIndex = np.where(symbolList == stocks.symbol)
-    referencePrice = history[symbolIndex[0][0],1]
-    gain = (price - float(referencePrice)) / float(referencePrice)
-    if gain < -0.05:
-        print("BUY " + stocks.symbol + " " + str("%2f" % gain) + " " + str(price))
-        # firstBuys[i,2] = price
-        # firstBuys = np.loadtxt('initialBuys.txt', dtype = str, skiprows = 1, delimiter=',')
-        # np.savetxt('initialBuys.txt', firstBuys,fmt='%s', delimiter= ',')
-    if stocks.initialBuys != "NA":
-        print(stocks.symbol + " " + str("%2f" % gain) + " OWNED")
-    else:
-        print(stocks.symbol + " " + str("%2f" % gain))
+
+def analyzer():
+    os.chdir(r'/home/bnichola/Python/Stock_Scraper')
+    firstBuys = np.loadtxt('initialBuys.txt', dtype = str, delimiter=',')
+    symbols = firstBuys[:,1]
+    initialBuys = firstBuys[:,2]
+    #prices = history[:,1]
+    #times = history[:,2]
+    buyAndSell = []
+    for i in range(0, len(symbols)):
+        try:
+            urls = "https://finance.yahoo.com/quote/" + symbols[i] + "?p=" + symbols[i] + "&.tsrc=fin-srch"
+            stocks = stock(urls, symbols[i], initialBuys[i])
+            price = stocks.priceScraper(stocks.url)
+            if stocks.initialBuys != "NA":
+                gain = (price - float(stocks.initialBuys)) / float(stocks.initialBuys)
+                print(stocks.symbol + " " + str("%2f" % gain) + " OWNED")
+                #print (stocks.symbol + " " + str(price) + " " + str(gain))
+                if gain > 0.05:
+                    print("SELL " + stocks.symbol + " " + str("%2f" % gain) + " " + str(price))
+                    buyAndSell.append([stocks.symbol, "SELL"])
+            stocks.historyBuilder(stocks.symbol, price)
+            history = np.loadtxt('stockhistory.txt', dtype = str, skiprows = 1, delimiter = ',')
+            symbolList = history[:,0]
+            symbolIndex = np.empty
+            symbolIndex = np.where(symbolList == stocks.symbol)
+            limit = int(len(symbolIndex[0]/5))
+            referencePrices = np.array(history[symbolIndex[0][:limit],1]).astype(np.float)
+            avgReferencePrice = sum(referencePrices) / limit
+            gain = (price - avgReferencePrice) / avgReferencePrice
+            if gain < -0.05:
+                print("BUY " + stocks.symbol + " " + str("%2f" % gain) + " " + str(price))
+                buyAndSell.append([stocks.symbol, "BUY"])
+            if stocks.initialBuys == "NA":
+                print(stocks.symbol + " " + str("%2f" % gain))
+            # for i in buyAndSell:
+            #     if i[1] == "SELL":
+            #         yOrN = input("Did you SELL" + i[0]+ "? ")
+            #         if yOrN == "Y":
+            #             index = np.where(symbols == i[0])
+            #             firstBuys[index[0][0],2] = "NA"
+            #             np.savetxt('initialBuys.txt', firstBuys, fmt = '%s', delimiter=',')
+            #     elif i[1] == "BUY":
+            #         yOrN = input("Did you BUY" + i[0]+ "? ")
+            #         if yOrN == "Y":
+            #             newBuy = input("How much? ")
+            #             index = np.where(symbols == i[0])
+            #             firstBuys[index[0][0],2] = newBuy
+            #             np.savetxt('initialBuys.txt', firstBuys, fmt = '%s', delimiter=',')
 
 
 
-# def toBuy():
-#     for i in range(0, len(symbols)):
-#         stocks = stock(urls[i], symbols[i], initialBuys[i])
-#         price = stocks.priceScraper(stocks.url)
-#         stocks.historyBuilder(stocks.symbol, price)
-#         history = np.loadtxt('stockhistory.txt', dtype = str, delimiter = ',')
-#         referencePrice = 
+
+        # def toBuy():
+        #     for i in range(0, len(symbols)):
+        #         stocks = stock(urls[i], symbols[i], initialBuys[i])
+        #         price = stocks.priceScraper(stocks.url)
+        #         stocks.historyBuilder(stocks.symbol, price)
+        #         history = np.loadtxt('stockhistory.txt', dtype = str, delimiter = ',')
+        #         referencePrices = 
+        except:
+            print("Failed to scrape for {0}".format(stocks.symbol))
--- scraper.py
+++ scraper.py
@@ -2,6 +2,9 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+from datetime import timedelta
+import numpy as np
+import time 
 
 class stock():
 
@@ -27,4 +30,23 @@ class stock():
         else:
             with open ('stockhistory.txt', 'a') as file:
                 file.write(self.symbol + "," + str(price) + "," + str(datetime.now())+ '\n')
-    
+
+def nearest(items, pivot):
+    return min(items, key=lambda x: abs(x - pivot))  
+
+def averageCalculator(symbol, timescale, delta):
+    history = np.loadtxt('stockhistory.txt', dtype = str, skiprows = 1, delimiter = ',')
+    fmt = '%Y-%m-%d %H:%M:%S.%f'
+    symbols = history[:,0]
+    symbolindex = np.empty
+    symbolIndex = np.where(symbols == symbol)
+    prices = np.array(history[symbolIndex[0][:],1]).astype(np.float)
+    times = np.array(history[symbolIndex[0][:],2]).astype(datetime)
+    for i in range (0, len(times)):
+        times[i] = datetime.strptime(times[i], fmt)
+    upperBound = datetime.now() - timedelta(**{timescale: delta})
+    lowerBound = upperBound - timedelta(**{timescale: delta})
+    priceRange = prices[np.where(times == nearest(times,lowerBound))[0][0]:np.where(times == nearest(times,upperBound))[0][0]]
+    averagePrice = sum(priceRange) / len(priceRange)
+    print(averagePrice)
+averageCalculator("TSLA", "weeks", 2)
